65a1c997d3cbd7765e7a77ede31fe9569931a054
JOhnwick4413 <www.akashabalaji@gmail.com>
2025-06-08 18:55:50 +0530

    hello world changes

---
+++ 01.hello-world.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This is a simple shell script that prints a series of messages to the console.
+echo "Hello, World!"
+echo "This is my first shell script"
+echo "I am learning shell scripting."
+echo "I am excited to learn more!"
+echo "I hope to become proficient in shell scripting."
+echo "I will practice regularly to improve my skills."
+echo "Thank you for watching my first shell script!"
