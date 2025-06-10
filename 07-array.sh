2e72f033da28d5c2ab5c624ff00ef0b5d1cf9a06
JOhnwick4413 <www.akashabalaji@gmail.com>
2025-06-10 18:30:35 +0530

    array

--- 07-array.sh
+++ 07-array.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 MOVIES=("Inception" "Interstellar" "The Dark Knight" "Memento" "Dunkirk")
 echo "my first movie is: ${MOVIES[0]}"
-echo "my second movie is: ${MOVIES[3]}"
-echo "my third movie is: ${MOVIES[4]}"
+echo "my second movie is: ${MOVIES[1]}"
+echo "my third movie is: ${MOVIES[2]}"
+# shellcheck disable=SC2145
 echo "all movies:${MOVIES[@]}"
