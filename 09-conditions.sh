8c2488a8f9452f5f42b5ee3b57a18af6e9232523
akashabalaji <akashabalaji123@gmail.com>
2025-06-25 18:33:35 +0530

    all changes

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,6 +2,11 @@
 #  write a program if given number is less then 10 or not
 NUMBER=$1
 
+if ! [[ "$NUMBER" =~ ^-?[0-9]+$ ]]; then
+    echo "Please provide a valid integer as an argument."
+    exit 1
+fi
+
 if [ "$NUMBER" -lt 10 ]
 then
     echo "The given number is less than 10"
